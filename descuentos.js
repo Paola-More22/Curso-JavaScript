9ea7bcffc1a97f6559a90e9cc43d24a2f6d18124
Angy More <angypao2122@gmail.com>
2022-05-02 16:31:34 -0500

    Anexando cupon de descuento

--- descuentos.js
+++ descuentos.js
@@ -15,13 +15,39 @@ function validarVacio(value){
     return true
 }
 
+const cupon = [
+    {
+        nombre: "JuanDC_es_Batman",
+        descuento: 15,
+    },
+    {
+        nombre: "pero_no_le_digas_a_nadie",
+        descuento: 30,
+    },
+    {
+        nombre: "es_un_secreto",
+        descuento: 25,
+    },
+];
+
 function clickPrecioConDescuento(){
-    const precio = document.getElementById("inputPrecio");
-    if(!validarVacio(precio.value)) alert("Por favor digite precio");
-    const descuento = document.getElementById("inputDescuento");
-    if(!validarVacio(descuento.value)) alert("Por favor digite descuento");
-    const precioP = parseInt(precio.value);
-    const descuentoD = parseInt(descuento.value);
-    const precioConDescuento = calcularPrecioConDescuento(precioP, descuentoD);
-    document.getElementById("inputResultado").value = ("El precio con descuento " + precioConDescuento);
+    const inputPrecio = document.getElementById("inputPrecio");
+    const precioValue = parseInt(inputPrecio.value);
+
+    const InputCupon = document.getElementById("InputCupon");
+    const cuponValue = InputCupon.value;
+
+    const isCuponValueValid = function (cupon) {
+        return cupon.nombre === cuponValue;
+    };
+
+    const userCupon = cupon.find(isCuponValueValid);
+
+    if (!userCupon) {
+        document.getElementById("inputResultado").value = ("El cupon: " + cuponValue + " No es valido");
+    } else {
+        const descuento = userCupon.descuento;
+        const precioConDescuento = calcularPrecioConDescuento(precioValue, descuento, cuponValue);
+        document.getElementById("inputResultado").value = ("El precio con cupon es: " + precioConDescuento);
+    }
 }
